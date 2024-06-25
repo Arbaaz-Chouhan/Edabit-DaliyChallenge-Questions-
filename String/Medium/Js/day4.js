da222bb9923ee5bcf1d58a3563ce03d4e8284cf2
PC <chouhanarbaaz4@gmail.com>
2024-06-25 17:20:26 +0530

    added day 4

---
+++ String/Medium/Js/day4.js
@@ -0,0 +1,30 @@
+// 41 =>  String Slice-athon
+// This challenge has five miniature exercises to help practice proficiency in string slicing. Check the examples below for a visual indicator of how to slice the strings. Good luck!
+
+function challenge1(s) {
+    return s.slice(0, 5);
+}
+
+function challenge2(s) {
+    return s.slice(-5)
+}
+
+function challenge3(s){
+    let res = "";
+    for(let )
+}
+console.log(challenge1("abcdefghijklmnopqrstuvwxyz"
+)); // "abcde"
+// First 5 characters of the string."
+console.log(challenge2("abcdefghijklmnopqrstuvwxyz"
+)); // "vwxyz"
+// // Last 5 characters of the string."
+console.log(challenge3("abcdefghijklmnopqrstuvwxyz"
+)); // "zyxwvutsrqponmlkjihgfedcba"
+// // // All characters in the string from back."
+// console.log(challenge4("abcdefghijklmnopqrstuvwxyz"
+// )); // "fedcba"
+// // // First 6 characters in the string (start with 6th character and go backwards)."
+// console.log(challenge5("abcdefghijklmnopqrstuvwxyz"
+// )); // "tvxz"
+// // // Take last 7 characters and only return odd positioned ones.
