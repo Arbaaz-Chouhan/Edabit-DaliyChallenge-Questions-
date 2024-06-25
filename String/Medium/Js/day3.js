d78af741adde964347943647d8854a26a45ffaf1
PC <chouhanarbaaz4@gmail.com>
2024-06-25 23:16:26 +0530

    in this repo we solve daily random question of javascript

--- String/Medium/Js/day3.js
+++ String/Medium/Js/day3.js
@@ -112,3 +112,118 @@ function boomIntensity(num) {
 // // There are 5 "o"s and 5 is divisible by 5 (all caps)
 // console.log(boomIntensity(10));  // "BOOOOOOOOOOM!"
 // // There are 10 "o"s and 10 is divisible by 2 and 5 (all caps and exclamation mark included)
+
+
+// 36 => Filter Repeating Character Strings
+// Create a function that keeps only strings with repeating identical characters (in other words, it has a set size of 1).
+function identicalFilter(arr) {
+    let res = [];
+    for (let i = 0; i < arr.length; i++) {
+        let isIdentical = true;
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] !== arr[i][0]) {
+                isIdentical = false;
+                break;
+            }
+        }
+        if (isIdentical) {
+            res.push(arr[i]);
+        }
+    }
+    return res;
+}
+
+// console.log(identicalFilter(["aaaaaa", "bc", "d", "eeee", "xyz"])); // ["aaaaaa", "d", "eeee"]
+// console.log(identicalFilter(["88", "999", "22", "545", "133"])); // ["88", "999", "22"]
+// console.log(identicalFilter(["xxxxo", "oxo", "xox", "ooxxoo", "oxo"])); // []
+
+
+// 37 => Adding Parity Bits
+// Parity bits are used as a very simple checksum to ensure that binary data isn't corrupted during transit. Here's how they work:
+
+// If a binary string has an odd number of 1's, the parity bit is a 1.
+// If a binary string has an even number of 1's, the parity bit is a 0.
+// The parity bit is appended to the end of the binary string.
+// Create a function that adds the correct parity bit to a binary string.
+
+function addParityBit(num) {
+    let countOnes = 0;
+
+    // Count the number of '1's in the binary string
+    for (let i = 0; i < num.length; i++) {
+        if (num[i] === '1') {
+            countOnes++;
+        }
+    }
+
+    // Determine the parity bit based on the count of '1's
+    let parityBit = (countOnes % 2 === 1) ? '1' : '0';
+
+    // Append the parity bit to the binary string
+    return num + parityBit;
+}
+
+// console.log(addParityBit("1011011")); // "10110111"
+// // There are five 1's.
+// // Since five is odd, the parity bit should be a 1.
+// // Add the parity bit to the end of the string.
+// // Return the result.
+// console.log(addParityBit("0110000")); // "01100000"
+// console.log(addParityBit("0101101")); // "01011010"
+// console.log(addParityBit("1111111")); // "11111111"
+
+
+
+// 38 => Same ASCII?
+// Return true if the sum of ASCII values of the first string is same as the sum of ASCII values of the second string, otherwise return false.
+
+function sameAscii(s1, s2) {
+    let s1sum = 0;
+    let s2sum = 0;
+
+    for (let i = 0; i < s1.length; i++) {
+        let s1words = s1[i].charCodeAt();
+        let s2words = s2[i].charCodeAt();
+        s1sum += s1words;
+        s2sum += s2words;
+
+    }
+    return s1sum === s2sum;
+}
+
+// console.log(sameAscii("a", "a")); // true
+// console.log(sameAscii("AA", "B@")); // true
+// console.log(sameAscii("EdAbIt", "EDABIT")); // false
+
+
+// 39 => Fractions and Rounding
+// Given a fraction frac (given in the format "1/2" for example) and n number of decimal places, return a sentence in the following format:
+
+// "{fraction} rounded to {n} decimal places is {answer}"
+
+function fracRound(strnum, num) {
+    let total = eval(strnum);
+    return `${strnum} rounded to ${num} decimal places is ${total.toFixed(num)}`
+}
+
+
+// console.log(fracRound("1/3", 5)); // "1/3 rounded to 5 decimal places is 0.33333"
+// console.log(fracRound("2/8", 4)); // "2/8 rounded to 4 decimal places is 0.2500"
+// console.log(fracRound("22/7", 2)); // "22/7 rounded to 2 decimal places is 3.14"
+
+// 40 => Dashed Vowels
+// Create a function that takes a string and returns dashes on the left and right side of every vowel (a e i o u).
+
+function dashed(str) {
+    let res = [];
+    for (let i = 0; i < str.length; i++) {
+        res.push(str[i]);
+    }
+   
+    
+    return res.join("-");
+}
+
+// console.log(dashed("Edabit")); // "-E-d-a-b-i-t"
+// console.log(dashed("Carpe Diem")); // "C-a-rp-e- D-i--e-m"
+// console.log(dashed("Fight for your right to party!")); // "F-i-ght f-o-r y-o--u-r r-i-ght t-o- p-a-rty!""
--- String/Medium/Js/day4.js
+++ String/Medium/Js/day4.js
@@ -9,22 +9,56 @@ function challenge2(s) {
     return s.slice(-5)
 }
 
-function challenge3(s){
+function challenge3(s) {
     let res = "";
-    for(let )
+    for (let i = s.length - 1; i >= 0; i--) {
+        res += s[i];
+    }
+    return res
 }
-console.log(challenge1("abcdefghijklmnopqrstuvwxyz"
-)); // "abcde"
-// First 5 characters of the string."
-console.log(challenge2("abcdefghijklmnopqrstuvwxyz"
-)); // "vwxyz"
-// // Last 5 characters of the string."
-console.log(challenge3("abcdefghijklmnopqrstuvwxyz"
-)); // "zyxwvutsrqponmlkjihgfedcba"
-// // // All characters in the string from back."
+
+function challenge4(s) {
+    let res = "";
+    for (let i = s.length - 21; i >= 0; i--) {
+        res += s[i];
+    }
+    return res
+}
+
+function challenge5(s) {
+    let res = "";
+    let last7char = s.slice(-7)
+    for (let i = 0; i < last7char.length; i++) {
+        if ((i+1) % 2 === 1) {
+            res += last7char[i];
+        }
+    }
+    return res
+}
+
+// console.log(challenge1("abcdefghijklmnopqrstuvwxyz"
+// )); // "abcde"
+// // First 5 characters of the string."
+// console.log(challenge2("abcdefghijklmnopqrstuvwxyz"
+// )); // "vwxyz"
+// // // Last 5 characters of the string."
+// console.log(challenge3("abcdefghijklmnopqrstuvwxyz"
+// )); // "zyxwvutsrqponmlkjihgfedcba"
+// // // // All characters in the string from back."
 // console.log(challenge4("abcdefghijklmnopqrstuvwxyz"
 // )); // "fedcba"
 // // // First 6 characters in the string (start with 6th character and go backwards)."
 // console.log(challenge5("abcdefghijklmnopqrstuvwxyz"
 // )); // "tvxz"
 // // // Take last 7 characters and only return odd positioned ones.
+
+
+// 42 => The Reverser!
+// The "Reverser" takes a string as input and returns that string in reverse order, with the opposite case.
+
+
+// reverse("Hello World") ➞ "DLROw OLLEh"
+
+// reverse("ReVeRsE") ➞ "eSrEvEr"
+
+// reverse("Radar") ➞ "RADAr"
