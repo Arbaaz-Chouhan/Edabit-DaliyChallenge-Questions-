e0ef25b5cf05c774f562197b15f0a6c8a562493d
PC <chouhanarbaaz4@gmail.com>
2024-07-28 23:34:31 +0530

    Edabit-Daiy-Challenge-Questions

--- String/Medium/Js/day12.js
+++ String/Medium/Js/day12.js
@@ -26,71 +26,33 @@ function replace(str1, str2) {
 // console.log(replace("microscopic", "i-i")); // "m#croscop#c"
 // console.log(replace("", "a-z")); // ""
 
-// Strategy Interface
-class PaymentStrategy {
-    pay(amount) {
-        throw new Error("This method should be overridden");
+
+ // 132 => Number of Two or More Consecutive Ones
+// Create a function that counts the number of blocks of two or more adjacent 1s in an array.
+
+ 
+function countOnes(arr) {
+    let count = 0;
+    let consecutiveOnes = 0;
+  
+    for (let i = 0; i <= arr.length; i++) {
+      if (arr[i] === 1) {
+        consecutiveOnes++;
+      } else {
+        if (consecutiveOnes >= 2) {
+          count++;
+        }
+        consecutiveOnes = 0;
+      }
     }
-}
+  
+    return count;
+  }
+  
+//   console.log(countOnes([1, 0, 0, 1, 1, 0, 1, 1, 1])); // ➞ 2
+//   console.log(countOnes([1, 0, 1, 0, 1, 0, 1, 0])); // ➞ 0
+//   console.log(countOnes([1, 1, 1, 1, 0, 0, 0, 0])); // ➞ 1
+//   console.log(countOnes([0, 0, 0])); // ➞ 0
+  
 
-// Concrete Strategy 1
-class CreditCardPayment extends PaymentStrategy {
-    constructor(cardNumber, cvv, expiryDate) {
-        super();
-        this.cardNumber = cardNumber;
-        this.cvv = cvv;
-        this.expiryDate = expiryDate;
-    }
-
-    pay(amount) {
-        console.log(`Paid ${amount} using Credit Card ending with ${this.cardNumber.slice(-4)}`);
-    }
-}
-
-// Concrete Strategy 2
-class PayPalPayment extends PaymentStrategy {
-    constructor(email) {
-        super();
-        this.email = email;
-    }
-
-    pay(amount) {
-        console.log(`Paid ${amount} using PayPal with email ${this.email}`);
-    }
-}
-
-// Context Class
-class ShoppingCart {
-    constructor() {
-        this.items = [];
-        this.paymentStrategy = null;
-    }
-
-    addItem(item) {
-        this.items.push(item);
-    }
-
-    setPaymentStrategy(paymentStrategy) {
-        this.paymentStrategy = paymentStrategy;
-    }
-
-    checkout() {
-        const amount = this.items.reduce((total, item) => total + item.price, 0);
-        this.paymentStrategy.pay(amount);
-    }
-}
-
-// Usage
-const cart = new ShoppingCart();
-cart.addItem({ name: 'Laptop', price: 1000, });
-cart.addItem({ name: 'Mouse', price: 50 });
-
-// Set Payment Strategy to Credit Card
-const creditCardPayment = new CreditCardPayment('1234567890123456', '123', '12/23');
-cart.setPaymentStrategy(creditCardPayment);
-cart.checkout(); // Output: Paid 1050 using Credit Card ending with 3456
-
-// Set Payment Strategy to PayPal
-const payPalPayment = new PayPalPayment('user@example.com');
-cart.setPaymentStrategy(payPalPayment);
-cart.checkout(); // Output: Paid 1050 using PayPal with email user@example.com
+  
