78e8812db01128701bee34193dda0dc77d4634a0
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2024-10-23 23:49:26 +0530

    objects-quesions

--- Objects/Very-Easy/JS/day1.js
+++ Objects/Very-Easy/JS/day1.js
@@ -54,13 +54,13 @@ function fiftyThirtyTwenty(ati) {
 // Han	brother in law
 // R2D2	droid
 
-function relationToLuke(str){
+function relationToLuke(str) {
 
-    if(str === "Darth Vader"){
+    if (str === "Darth Vader") {
         return "Luck I am your father";
-    }else if(str === "Leia"){
+    } else if (str === "Leia") {
         return "Luck I am your sister";
-    }else if(str === "Han"){
+    } else if (str === "Han") {
         return "Luck I am your brother in law"
     }
 }
@@ -84,4 +84,88 @@ function relationToLuke(str) {
 // console.log(relationToLuke("Han")); // "Luke, I am your brother in law."
 
 
-// 5 => 
+// 5 => Extract City Facts
+// Create a function that takes an object as an argument and returns a string with facts about the city. The city facts will need to be extracted from the object's three properties:
+
+// name
+// population
+// continent
+// The string should have the following format: X has a population of Y and is situated in Z (where X is the city name, Y is the population and Z is the continent the city is situated in).
+
+function cityFacts(obj) {
+    let keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i++) {
+        let currentValues = obj[keys[i]]
+        return `${currentValues[0]} has a population of ${currentValues[1]} and is situated in ${currentValues[2]}`
+    }
+}
+
+
+console.log(cityFacts({
+    name: "Paris",
+    population: "2,140,526",
+    continent: "Europe"
+})); // "Paris has a population of 2,140,526 and is situated in Europe"
+
+console.log(cityFacts({
+    name: "Tokyo",
+    population: "13,929,286",
+    continent: "Asia"
+})); // "Tokyo has a population of 13,929,286 and is situated in Asia"
+
+
+// 6 Classes For Fetching Information on a Sports Player
+// Create a class that takes the following four arguments for a particular football player:
+
+// name
+// age
+// height
+// weight
+// Also, create three functions for the class that returns the following strings:
+
+// getAge() returns "name is age age"
+// getHeight() returns "name is heightcm"
+// getWeight() returns "name weighs weightkg"
+
+
+class Player {
+    constructor(name, age, height, weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    getAge() {
+        return `${this.name} is age ${this.age}`;
+    }
+
+    getHeight() {
+        return `${this.name} is ${this.height}cm`;
+    }
+
+    getWeight() {
+        return `${this.name} weighs ${this.weight}kg`;
+    }
+}
+
+// Example usage:
+let p1 = new Player("David Jones", 25, 175, 75);
+
+// console.log(p1.getAge());    // "David Jones is age 25"
+// console.log(p1.getHeight()); // "David Jones is 175cm"
+// console.log(p1.getWeight()); // "David Jones weighs 75kg"
+
+
+
+// 7  Check if Number is within a Given Range
+// Given a number and an object with min and max properties, return true if the number lies within the given range (inclusive).
+
+function isInRange(num, range) {
+    return num >= range.min && num <= range.max;
+}
+
+console.log(isInRange(4, { min: 0, max: 5 })); /// true
+console.log(isInRange(4, { min: 4, max: 5 })); /// true
+console.log(isInRange(4, { min: 6, max: 10 })); /// false
+console.log(isInRange(5, { min: 5, max: 5 })); /// true 
