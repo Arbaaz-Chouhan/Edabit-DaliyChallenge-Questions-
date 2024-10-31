2edc9e5d2952dcd006ec880d66d0f8cc311e2d24
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2024-10-31 23:55:54 +0530

    Edabit-question

--- Objects/Very-Easy/JS/day1.js
+++ Objects/Very-Easy/JS/day1.js
@@ -165,7 +165,8 @@ function isInRange(num, range) {
     return num >= range.min && num <= range.max;
 }
 
-console.log(isInRange(4, { min: 0, max: 5 })); /// true
-console.log(isInRange(4, { min: 4, max: 5 })); /// true
-console.log(isInRange(4, { min: 6, max: 10 })); /// false
-console.log(isInRange(5, { min: 5, max: 5 })); /// true 
+// console.log(isInRange(4, { min: 0, max: 5 })); /// true
+// console.log(isInRange(4, { min: 4, max: 5 })); /// true
+// console.log(isInRange(4, { min: 6, max: 10 })); /// false
+// console.log(isInRange(5, { min: 5, max: 5 })); /// true 
+
