11ce13a23738299bb4915b06cd0d0b322056256e
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2024-11-07 23:56:50 +0530

    Edabit-questions

--- Objects/Easy/JS/day1.js
+++ Objects/Easy/JS/day1.js
@@ -123,7 +123,7 @@ function addName(obj, name, value) {
     obj[name] = value;
     return obj;
 
-xc}
+}
 
 console.log(addName({}, "Brutus", 300)); // { Brutus: 300 }
 console.log(addName({ piano: 500 }, "Brutus", 400)); // { piano: 500, Brutus: 400 }
