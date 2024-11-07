f1c370942a711f5be03cc576e8cbbb5a45c6bd62
Arbaaz chouhan <chouhanarbaaz4@gmail.com>
2024-11-07 23:55:11 +0530

    Edabit-questions

---
+++ Objects/Easy/JS/day1.js
@@ -0,0 +1,130 @@
+// 1 =>  Slightly Superior
+// You will be given two extremely similar arrays, but exactly one of the items in an array will be valued slightly higher than its counterpart (which means that evaluating the value > the other value will return true).
+
+// Create a function that returns whether the first array is slightly superior to that of the second.
+
+function isFirstSuperior(arr1, arr2) {
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] > arr2[i]) {
+            return true;
+        } else if (arr1[i] < arr2[i]) {
+            return false;
+        }
+    }
+    return false;
+}
+
+
+// console.log(isFirstSuperior([1, 2, 4], [1, 2, 3])); //  true
+// console.log(isFirstSuperior(["a", "d", "c"], ["a", "b", "c"])); //  true
+// console.log(isFirstSuperior(["zebra", "ostrich", "whale"], ["ant", "ostrich", "whale"])); //  true
+// console.log(isFirstSuperior([1, 2, 3, 4], [1, 2, 4, 4])); //  false
+// console.log(isFirstSuperior([true, 10, "zebra"], [true, 10, "zebra"])); //  false
+
+
+// 2 => Burglary Series (10): Calculate Difference
+// The insurance guy calls again and apologizes. They found another policy made by your spouse, but this one is limited to cover a particular maximum in losses (for example, 50,000€). You send a bill to your spouse for the difference you lost.
+
+// Given an object of the stolen items and a limit, return the difference between the total value of those items and the limit of the policy.
+
+function calculateDifference(obj, limit) {
+    let sum = 0;
+    let keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i++) {
+        sum += obj[keys[i]];
+    }
+
+    return sum - limit
+}
+
+// console.log(calculateDifference({ "baseball bat": 20 }, 5));  // 15
+// console.log(calculateDifference({ skate: 10, painting: 20 }, 19)); //  11
+// console.log(calculateDifference({ skate: 200, painting: 200, shoes: 1 }, 400)); //  1
+
+// 3 => Scrabble Hand
+// Given an array of scrabble tiles, create a function that outputs the maximum possible score a player can achieve by summing up the total number of points for all the tiles in their hand. Each hand contains 7 scrabble tiles.
+
+// Here's an example hand:
+
+// [
+//   { tile: "N", score: 1 },
+//   { tile: "K", score: 5 },
+//   { tile: "Z", score: 10 },
+//   { tile: "X", score: 8 },
+//   { tile: "D", score: 2 },
+//   { tile: "A", score: 1 },
+//   { tile: "E", score: 1 }
+// ]
+// The players maximumScore from playing all these tiles would be 1 + 5 + 10 + 8 + 2 + 1 + 1, or 28.
+
+function maximumScore(arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i].score;
+    }
+    return sum
+}
+
+// console.log(
+//     maximumScore([
+//         { tile: "N", score: 1 },
+//         { tile: "K", score: 5 },
+//         { tile: "Z", score: 10 },
+//         { tile: "X", score: 8 },
+//         { tile: "D", score: 2 },
+//         { tile: "A", score: 1 },
+//         { tile: "E", score: 1 }
+//     ]));  // 28
+
+// console.log(maximumScore([
+//     { tile: "B", score: 2 },
+//     { tile: "V", score: 4 },
+//     { tile: "F", score: 4 },
+//     { tile: "U", score: 1 },
+//     { tile: "D", score: 2 },
+//     { tile: "O", score: 1 },
+//     { tile: "U", score: 1 }
+// ])); ; // 15
+
+
+// 4  => Printer Levels
+// Given an object of how many more pages each ink color can print, output the maximum number of pages the printer can print before any of the colors run out.
+function inkLevels(obj) {
+
+    let minInkLevel = Math.min(...Object.values(obj));  // Find the minimum value in the object
+    return minInkLevel;
+}
+
+// console.log(
+//  inkLevels({
+//   "cyan": 23,
+//   "magenta": 12,
+//   "yellow": 10
+// })); // 10
+
+// console.log(
+//  inkLevels({
+//   "cyan": 432,
+//   "magenta": 543,
+//   "yellow": 777
+// })); // 432
+
+// console.log(
+//  inkLevels({
+//   "cyan": 700,
+//   "magenta": 700,
+//   "yellow": 0
+// })); // 0
+
+// 5 =>  Burglary Series (04): Add its Name
+// Given three arguments ⁠— an object obj of the stolen items, the pet's name and a value ⁠— return an object with that name and value in it (as key-value pairs).
+
+function addName(obj, name, value) {
+    obj[name] = value;
+    return obj;
+
+xc}
+
+console.log(addName({}, "Brutus", 300)); // { Brutus: 300 }
+console.log(addName({ piano: 500 }, "Brutus", 400)); // { piano: 500, Brutus: 400 }
+console.log(addName({ piano: 500, stereo: 300 }, "Caligula", 440)); // { piano: 500, stereo: 300, Caligula: 440 }
--- Objects/Very-Easy/day1.js
+++ Objects/Very-Easy/day1.js
@@ -170,3 +170,129 @@ function isInRange(num, range) {
 // console.log(isInRange(4, { min: 6, max: 10 })); /// false
 // console.log(isInRange(5, { min: 5, max: 5 })); /// true 
 
+// 8 => Return Sole Element in a Set
+// Given a set containing an element, return the sole element.
+
+function elementSet(set) {
+    return [...set.values()][0]; // Convert the set to an array and return the first element
+}
+
+// Example usage
+const first = new Set();
+first.add(1);
+// console.log(elementSet(first)); // 1
+
+// const second = new Set();
+// second.add("apple");
+// console.log(elementSet(second)); // "apple"
+
+// const third = new Set();
+// third.add(false);
+// console.log(elementSet(third)); // false
+
+
+// 9 Burglary Series (11): Say What
+// The insurance guy calls again. Apparently, they were informed by your spouse that some items were not stolen at all and you failed to mention this detail to them. This is a fraud attempt! You freeze and mumble something unintelligible. Find out what you said.
+
+// Given an object, return a string that concatenates all the values and adds the 2nd key at the end. Make sure you keep an empty space between them but not at the beginning or end of the string. Look at the examples for a clearer picture.
+
+function sayWhat(obj) {
+    let values = Object.values(obj);
+    return valuess.join(" ") + " " + values[1]
+}
+
+// console.log(sayWhat({ 1: "Mommy", 2: "please", 3: "help" })); // "Mommy please help please"
+// console.log(sayWhat({ 1: "Me", 2: "innocent", 3: "is" })); // "Me innocent is innocent"
+// console.log(sayWhat({ 1: "Must", 2: "lawyer", 3: "call" })); // "Must lawyer call lawyer"
+
+
+// 10 => Find the Bug: Returning Valid Units of Measure
+// There has been a masterdata issue which affected the unit of measure of the products. All values need to be checked if they are valid. The unit of measure is valid when it is either "L" (liters), "PCE" (pieces) OR when the product has a comment.
+
+// The return value should be a Boolean.
+
+function hasValidUnitOfMeasure(obj) {
+    let values = Object.values(obj);
+
+    if (values[values.length - 1] === "") {
+        return false
+    }
+    if (values[values.length - 1] === "L") {
+        return true
+    }
+    return false
+}
+
+
+// console.log(hasValidUnitOfMeasure({ "product": "Milk", unitOfMeasure: "L" })); // true
+// console.log(hasValidUnitOfMeasure({ "product": "Cereals", unitOfMeasure: "" })); // false
+// console.log(hasValidUnitOfMeasure({ "product": "Beer", unitOfMeasure: false })); // false
+// console.log(hasValidUnitOfMeasure({ "product": "Beef", unitOfMeasure: "Cow" })); // false
+
+
+// 11 => Does the Object Contain a Given Key?
+// Write a function that returns true if a hash contains the specified key, and false otherwise.
+
+function hasKey(obj, key) {
+    let keys = Object.keys(obj);
+    for (let i = 0; i < keys.length; i++) {
+        if (keys[i] === key) {
+            return true
+        }
+    }
+    return false
+
+}
+
+// console.log(hasKey({ a: 44, b: 45, c: 46 }, "d")); // false
+// console.log(hasKey({ craves: true, midnight: true, snack: true }, "morning")); // false
+// console.log(hasKey({ pot: 1, tot: 2, not: 3 }, "not")); // true
+
+
+//12 => Calculate the Profit
+// You work for a manufacturer, and have been asked to calculate the total profit made on the sales of a product. You are given an object containing the cost price per unit (in dollars), sell price per unit (in dollars), and the starting inventory. Return the total console.log(
+// profit made, rounded to the nearest dollar.
+
+function profit(obj) {
+    const profitPerUnit = obj.sellPrice - obj.costPrice;      
+    const totalProfit = profitPerUnit * obj.inventory;        
+    return Math.round(totalProfit);  
+}
+
+// console.log(
+//     profit({
+//         costPrice: 32.67,
+//         sellPrice: 45.00,
+//         inventory: 1200
+//     })); // 14796
+
+// console.log(
+//     profit({
+//         costPrice: 225.89,
+//         sellPrice: 550.00,
+//         inventory: 100
+//     })); // 32411
+
+// console.log(
+//     profit({
+//         costPrice: 2.77,
+//         sellPrice: 7.95,
+//         inventory: 8500
+//     })); // 44030
+
+
+// 13 =>  Is the Object Empty?
+// Write a function that returns true if an object is empty, and false otherwise.
+
+function isEmpty(obj){
+   let keys =  Object.keys(obj);
+    
+   if(keys.length === 0){
+        return true
+    }
+    return false
+}
+
+// console.log(isEmpty({})); // true
+// console.log(isEmpty({ a: 1 })); // false
+
